353bc8942b6cd5e091049cc4e7e66929aa0308e2
reiyuchan <85351652+reiyuchan@users.noreply.github.com>
2022-03-26 13:17:04 +0200

    initial commit

---
+++ src/App.py
@@ -0,0 +1,43 @@
+from pytube import YouTube,Playlist
+
+class YT_DL:
+    def __init__(self, choice) -> None:
+        self.choice = choice
+
+    def dlSingle(self,singleUrl):
+        self.yt = YouTube(
+            singleUrl,
+            use_oauth=False,
+            allow_oauth_cache=False
+            )
+        print(f"Downloading video: {singleUrl}")
+        print(f"Download in progress...!")
+        self.yt.streams.filter(file_extension='mp4').first().download()
+
+    def setUP(self):
+        if self.choice == "s":
+            singleUrl = input("Enter the video url: ")
+            self.dlSingle(singleUrl)
+        elif self.choice == "m":
+            playlistUrl = input("Enter the playlist url: ")
+            self.dlPlaylist(playlistUrl)
+
+    def dlPlaylist(self,playlistUrl):
+        self.plyls = Playlist(playlistUrl)
+        for self.url in self.plyls.video_urls:
+            try:
+                self.yt
+            except self.VideoUnavailable:
+                print(f"Video {self.url} is unavailable, skipping...!")
+            else:
+                print(f"Downloading Video: {self.url}")
+                print(f"Download in progress...!")
+                self.video.streams.filter(file_extension='mp4').first().download()
+
+
+dl = YT_DL(
+    input(
+        "Do you want to download playlist or single video? (s for single video/m for playlist or alot of videos):"
+    )
+)
+dl.setUP()
